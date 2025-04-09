8885690100a506e7312d59788a2c69d09b924d94
Husnidin <husnidin@Husnidins-MacBook-Air.local>
2025-04-09 20:08:49 +0500

    remove static

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,22 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
-import { PrismaModule } from './prisma/prisma.module';
-import { FileUploadModule } from './file-upload/file-upload.module';
-import { MailModule } from './mail/mail.module';
-import { LanguageModule } from './language/language.module';
-import { AdminModule } from './admin/admin.module';
-import { ProductModule } from './product/product.module';
-import { AuthModule } from './auth/auth.module';
-import { CategoryModule } from './category/category.module';
-import { OrderModule } from './order/order.module';
-import { ContactModule } from './contact/contact.module';
+import { PrismaModule } from "./prisma/prisma.module";
+import { FileUploadModule } from "./file-upload/file-upload.module";
+import { MailModule } from "./mail/mail.module";
+import { LanguageModule } from "./language/language.module";
+import { AdminModule } from "./admin/admin.module";
+import { ProductModule } from "./product/product.module";
+import { AuthModule } from "./auth/auth.module";
+import { CategoryModule } from "./category/category.module";
+import { OrderModule } from "./order/order.module";
+import { ContactModule } from "./contact/contact.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "upload"),
-      serveRoot: "/static",
       serveStaticOptions: {
         index: false,
       },
